ac45f82a2f9b543f682d8f7d3849a9dd0563d2c2
unknownwind <kai19961201@gmail.com>
2016-11-14 19:22:16 +0800

    测试例子

---
+++ test.c
@@ -0,0 +1,30 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  test.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2016年11月14日 15时20分18秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Wind (wind), kai19961201@hotmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include "cJSON.h"
+
+int main(void)
+{
+    char *data = (char*)"{\"name\":\"hello\",\"list\":[{\"name\":\"hello\"},{\"name\":\"hello\"}]}";
+    cJSON *json = cJSON_Parse(data);
+    cJSON_Print(json);
+    printf("%s\n", data);
+    cJSON_Delete(json);
+    return 0;
+}
