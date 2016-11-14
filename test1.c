2f95b0b9c286f79b0bcc0cc6b059ad86ae83fab7
unknownwind <kai19961201@gmail.com>
2016-11-14 19:35:20 +0800

    update!

--- test1.c
+++ test1.c
@@ -16,4 +16,10 @@
  * =====================================================================================
  */
 #include <stdlib.h>
+#include <stdio.h>
 
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
